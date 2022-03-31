f3c435e4392906633695121768bfeab7de5c2840
Matt Lytle <mbl31@pitt.edu>
2022-03-31 13:14:23 -0400

    adding a main file

---
+++ BopIt_Main/BopIt_Main.ino
@@ -0,0 +1,14 @@
+// Defining constants for pins
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode();
+  pinMode();
+  pinMode();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
