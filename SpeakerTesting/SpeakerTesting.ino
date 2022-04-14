b509dcf127e51a323223d7eaa51d7eb942921737
Matt Lytle <mbl31@pitt.edu>
2022-04-14 16:17:38 -0400

    Tested TTS functionality

--- SpeakerTesting/SpeakerTesting.ino
+++ SpeakerTesting/SpeakerTesting.ino
@@ -1,4 +1,10 @@
-int Speaker = A3;
+#include "Talkie.h"
+#include "Vocab_US_Large.h"
+#include "Vocab_US_Acorn.h"
+
+
+Talkie voice;
+int Speaker = A0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,5 +13,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  tone(Speaker, 3,000);
+  /*tone(Speaker, 1,000);
+  delay(2000);
+  noTone(Speaker);
+  delay(2000);*/
+  voice.say(spa_THREE);
+  delay(1000);
+  voice.say(spa_TWO);
+  delay(1000);
+  voice.say(spa_ONE);
+  delay(1000);
+  voice.say(spa_START);
+  delay(2000);
 }
