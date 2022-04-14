53ee0415fa840fe2a63c629a4467c52a859739d3
Matt Lytle <mbl31@pitt.edu>
2022-04-13 21:26:30 -0400

    Testing the BCD output

---
+++ BCDTesting/BCDTesting.ino
@@ -0,0 +1,23 @@
+int bcdA = 7;
+int bcdB = 6;
+int bcdC = 5; 
+int bcdD = 4;
+byte BCD[10][4] = {{0,0,0,0},{0,0,0,1},{0,0,1,0},{0,0,1,1},{0,1,0,0},{0,1,0,1},{0,1,1,0},{0,1,1,1},{1,0,0,0},{1,0,0,1}}; // Used to convert the score into a BCD value
+int bcdInputs[4] = {bcdD, bcdC, bcdB, bcdA};
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 0; i < 4; i++) {
+    pinMode(bcdInputs[i], OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 4; i++) {
+    Serial.println(BCD[02][i]);
+    digitalWrite(bcdInputs[i], BCD[2][i]);
+  }
+  delay(3000);
+}
